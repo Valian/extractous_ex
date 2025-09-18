2e5e35f17ce5f29fd02cb24ebc0118b2efdf7c32
Valian <jakub.skalecki@gmail.com>
2025-09-18 11:21:02 +0200

    initial commit

---
+++ lib/extractous_ex.ex
@@ -0,0 +1,18 @@
+defmodule ExtractousEx do
+  @moduledoc """
+  Documentation for `ExtractousEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExtractousEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/extractous_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule ExtractousExTest do
+  use ExUnit.Case
+  doctest ExtractousEx
+
+  test "greets the world" do
+    assert ExtractousEx.hello() == :world
+  end
+end
